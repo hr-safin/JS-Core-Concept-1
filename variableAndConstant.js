b2a29e16d1ba5ad0a2350cdc69654bcd7e8c2cc0
unknown <hrsafin2434@gmail.com>
2023-12-29 10:39:07 +0600

    variable and constant

---
+++ variableAndConstant.js
@@ -0,0 +1,8 @@
+// variable and constant are used to store and manage data within the program
+// varibale can be reassgin but we cannot reasgin const 
+// variables are container for storing data values
+
+var a = "hasibur rahman safin"
+const b = "Mahi rahman "
+
+console.log(a + b)
