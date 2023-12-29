66d68e6eeb6eaf6ff5cab6de7432761787ec8036
unknown <hrsafin2434@gmail.com>
2023-12-29 18:44:52 +0600

    basic object

---
+++ object.js
@@ -0,0 +1,27 @@
+// object  > in object you can store and organize data in key value pairs
+// key value pairs
+
+
+// blank obj
+const obj = {}
+
+// -----------------------------------------------------------------
+
+
+// filled object 
+const info = {
+    name : "Hasibur rahman safin",
+    age : "21",
+    dream : "study abroad"
+}
+
+
+const name1 = info.name
+
+console.log(name1)
+
+console.log(info.age)
+
+const dream1 = info["dream"]
+
+console.log(dream1)
