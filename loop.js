ab16710a9cd5de0268910b353b3310a776b0ef5f
unknown <hrsafin2434@gmail.com>
2023-12-29 16:29:52 +0600

    for loop

---
+++ loop.js
@@ -0,0 +1,21 @@
+// loop -- for loop, while loop, do while loop
+
+// for(Start,end, change)
+
+// reduce repeatitive code
+
+
+// for(let i =0; i<11; i++){
+//     console.log("Hasibur rahman safin")
+// }
+
+// let array = [1,2,3,4,5,5]
+
+// for(let i=0; i<array.length; i++){
+//     console.log(array[i])
+// }
+
+
+for(let i =0; i<11; i++){
+    console.log("#".repeat(i))
+}
