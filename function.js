b0ab2065bfb32418d22c923787babd7d7562a0bd
unknown <hrsafin2434@gmail.com>
2023-12-29 18:04:27 +0600

    function and parameter vs arguments

---
+++ function.js
@@ -0,0 +1,7 @@
+// if no return is passed from function then function return undefined
+
+function sayHello(params){  // << this is called parameter also we can call placeholder
+    console.log(params, "is my love")
+}
+
+sayHello("Mahi")  // << this is called arguments that passed to the function
