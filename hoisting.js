a32ac80d298d56c9e1affcf3837d5d176df8bd8a
unknown <hrsafin2434@gmail.com>
2023-12-29 10:53:00 +0600

    hoisting

---
+++ hoisting.js
@@ -0,0 +1,6 @@
+// variable and function are hoisted which means their declaration moved to the top of the code
+var a // declaration > declaration moved to the top (only var and function) 
+
+console.log(a)
+
+a = "Hasibur rahman safin" // initialization
